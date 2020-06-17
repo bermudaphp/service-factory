01d272fcf4134b27a35dea0c5ba2c77b10774cc1
Andrey <alestro@bk.ru>
2020-06-17 14:48:18 +0200

    Update FactoryException.php

--- src/FactoryException.php
+++ src/FactoryException.php
@@ -1,21 +1,30 @@
 <?php
 
 
-namespace Lobster\Factory;
+namespace Lobster;
 
 
 /**
- * Class Exception
- * @package Lobster\Factory
+ * Class FactoryException
+ * @package Lobster
  */
-class FactoryException extends \RuntimeException implements Contracts\FactoryException
+class FactoryException extends \RuntimeException
 {
     /**
      * @param \Throwable $e
      * @return static
      */
-    public static function fromPrev(\Throwable $e) : self
+    public static function fromPrevios(\Throwable $e) : self
     {
         return new static($e->getMessage(), $e->getCode(), $e);
+    }
+    
+    /**
+     * @throws static
+     * @return void
+     */
+    public function throw() : void 
+    {
+        throw $this;
     }
 }
